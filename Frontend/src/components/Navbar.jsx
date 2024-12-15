6c6aa138a9b9ec4dab32f28d3069a760ce2df886
Shubham Das <subhamdas9800@gmail.com>
2024-12-15 22:46:44 +0530

    button

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -57,7 +57,7 @@ function Navbar() {
                 </div>
 
                 <div className="hidden md:flex items-center space-x-4">
-                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-2xl text-lg px-3 py-2 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">+ Upload</button>
+                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-3xl text-lg px-3 py-1 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">+ Upload</button>
                     <BellIcon className="w-6 h-6 text-gray-500" />
                     <CheckCircleIcon className="w-6 h-6 text-gray-500" />
                     <img src={Photo} alt="User" className="w-8 h-8 rounded-full" />
