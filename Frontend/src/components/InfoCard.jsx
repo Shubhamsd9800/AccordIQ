0e2d8fd77c8008928ab2f25b3c0da4cbd1acd52b
Shubham Das <subhamdas9800@gmail.com>
2024-12-15 22:35:46 +0530

    doneagain

--- Frontend/src/components/InfoCard.jsx
+++ Frontend/src/components/InfoCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function InfoCard({ title, value, subtitle }) {
   return (
     <div className="bg-white p-6 rounded-3xl shadow w-54">
-      <h3 className="text-gray-600 text-2xl">{title}</h3>
+      <h3 className="text-gray-600 text-2xl font-bold">{title}</h3>
       <p className="text-blue-800 text-3xl font-bold">{value}</p>
       {subtitle && <p className="text-gray-500 text-sm">{subtitle}</p>}
     </div>
