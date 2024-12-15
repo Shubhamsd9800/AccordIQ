71815e4ca8932283a9d77824250889eb2eabd06a
Shubham Das <subhamdas9800@gmail.com>
2024-12-15 17:49:38 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173/', // Update with your frontend URL
+  origin: 'https://accord-iq-dev.vercel.app/', // Update with your frontend URL
   credentials: true,
 }));
 
