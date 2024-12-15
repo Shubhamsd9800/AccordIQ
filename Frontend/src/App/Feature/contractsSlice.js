a2495dc6cf710ecbd3251a7a077c5de5c8501e1a
Shubham Das <subhamdas9800@gmail.com>
2024-12-15 17:46:41 +0530

    update

--- Frontend/src/App/Feature/contractsSlice.js
+++ Frontend/src/App/Feature/contractsSlice.js
@@ -4,7 +4,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 export const fetchContracts = createAsyncThunk(
   "contracts/fetchContracts",
   async () => {
-    const response = await fetch("https://accord-iq-backend.vercel.app/api/documents");
+    const response = await fetch("https://accord-iq-back.vercel.app/api/documents/");
 
     // Check for response errors
     if (!response.ok) {
